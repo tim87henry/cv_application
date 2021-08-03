af56ac4a7bba91392738d7ff07c4057205527974
Timothy <tim87henry@gmail.com>
2021-08-03 23:31:04 +1000

    Customised the personal details section

--- src/components/personal_details.js
+++ src/components/personal_details.js
@@ -14,37 +14,52 @@ class PersonalDetails extends React.Component {
     render() {
         let personal_info=[];
         if(this.props.editMode===true) {
-            personal_info=<p>
-                Name
-                <input type="input" name="name" value={this.props.name} onChange={this.handleInput}></input><br/><br/>
-                Email
-                <input type="input" name="email" value={this.props.email} onChange={this.handleInput}></input><br/><br/>
-                Phone No
-                <input type="input" name="phone" value={this.props.phone} onChange={this.handleInput}></input><br/><br/>
-                City
-                <input type="input" name="city" value={this.props.city} onChange={this.handleInput}></input><br/><br/>
-                County
-                <input type="input" name="country" value={this.props.country} onChange={this.handleInput}></input><br/><br/>
-            </p>
+            personal_info=<div class="personalDataForm">
+                <span>
+                    <label>Name</label>
+                    <input type="input" name="name" value={this.props.name} onChange={this.handleInput}></input>
+                </span>
+                <span>
+                    <label>Email</label>
+                    <input type="input" name="email" value={this.props.email} onChange={this.handleInput}></input>
+                </span>
+                <span>
+                    <label>Phone No</label>
+                    <input type="input" name="phone" value={this.props.phone} onChange={this.handleInput}></input>
+                </span>
+                <span>
+                    <label>City</label>
+                    <input type="input" name="city" value={this.props.city} onChange={this.handleInput}></input>
+                </span>
+                <span>
+                    <label>County</label>
+                    <input type="input" name="country" value={this.props.country} onChange={this.handleInput}></input>
+                </span>
+            </div>
         } else {
             personal_info=<div className="personalInfo">
                 <div className="userName">
                     {this.props.name}
                 </div>
                 <div className="userInfo">
-                    <span className="section1">
-                        <ul className="userInfoList">
-                            <li><b>Phone</b> {this.props.phone}</li>
-                            <li><b>Email</b> {this.props.email}</li>
-                        </ul>
-                    </span>
-                    <span className="section2">
-                        <ul className="userInfoList">
-                            <li><b>City</b> {this.props.city}</li>
-                            <li><b>Country</b> {this.props.country}</li>
-                        </ul>
-                    </span>
+                    <table className="userInfoTable">
+                        <tr>
+                            <th>Phone</th>
+                            <td>{this.props.phone}</td>
+                            <th>City</th>
+                            <td>{this.props.city}</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>{this.props.email}</td>
+                            <th>Country</th>
+                            <td>{this.props.country}</td>
+                        </tr>
+                    </table>      
                 </div>
+                <p>
+                    I am a highly competent IT professional with a proven track record in designing websites, networking and managing databases. I am eager to be challenged in order to grow and further improve my IT skills. My greatest passion is in life is using my technical know-how to benefit other people and organisations.    
+                </p>  
             </div>
         }
         return(
